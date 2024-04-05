272a2fe82ff10e1dd8582663b5741fd4c15592d2
LenSys <[omitted]>
2024-04-05 17:07:55 +0200

    update terms and conditions

--- src/app/terms-conditions/page.tsx
+++ src/app/terms-conditions/page.tsx
@@ -1,5 +1,13 @@
+import H1 from "@/components/h1";
 import React from "react";
 
 export default function TermsConditionsPage() {
-  return <main>Terms &amp; Conditions</main>;
+  return (
+    <main className="flex flex-col items-center pt-36 px-3">
+      <H1>Terms &amp; Conditions</H1>
+      <p className="mb-12 mt-7 text-2xl lg:text-3xl opacity-75">
+        Terms &amp; Conditions apply...
+      </p>
+    </main>
+  );
 }
