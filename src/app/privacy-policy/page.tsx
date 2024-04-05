f5ce40d88d3cbb008090ce47eab221aef1c1351a
LenSys <[omitted]>
2024-04-05 17:07:39 +0200

    update privacy policy info

--- src/app/privacy-policy/page.tsx
+++ src/app/privacy-policy/page.tsx
@@ -1,3 +1,12 @@
+import H1 from "@/components/h1";
+
 export default function PrivacyPolicyPage() {
-  return <main>Privacy Policy</main>;
+  return (
+    <main className="flex flex-col items-center pt-36 px-3">
+      <H1>Privacy Policy</H1>
+      <p className="mb-12 mt-7 text-2xl lg:text-3xl opacity-75">
+        Our Privacy Policy
+      </p>
+    </main>
+  );
 }
